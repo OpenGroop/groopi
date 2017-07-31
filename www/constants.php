9f96bb34ce606bf85a21e91be8a27c2d7b2f23e5
nanku-dev <dev@nancoo.org>
2017-07-31 17:52:03 -0400

    edit constants.php

--- www/constants.php
+++ www/constants.php
@@ -1,5 +1,7 @@
 <?php 
 
+define("SENSORDATA_DB", "sqlite:/srv/sqlite3/data/sensordata.db");
+define("REGISTER_DB", "sqlite:/srv/sqlite3/data/register.db");
 define("TEMP_C", "temp_c");
 define("TEMP_F", "temp_f");
 
