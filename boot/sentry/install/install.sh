746511e00e9b588590b746c120532e1d2ba49c62
nanku-dev <dev@nancoo.org>
2017-08-02 14:01:29 -0400

    more boot stuff

--- boot/sentry/install/install.sh
+++ boot/sentry/install/install.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-expandFilesystem()
 
 deluser --remove-home pi
 adduser --no-create-home --disabled-password --disabled-login --gecos "" sentry
@@ -18,12 +17,23 @@ pip install pyudev
 mkdir -pv /srv/sqlite3/data
 mkdir -v /var/www/public
 
+chown -v www-data:www-data /srv/sqlite3
+chown -v www-data:www-data /srv/sqlite3/data
+chown -v suser:suser /var/www/public
 
-# sudo iptables -A INPUT -p tcp -m tcp --dport 80  -j ACCEPT
-# sudo iptables -A INPUT -p tcp -m tcp --dport 443 -j ACCEPT
+cp -v /etc/lighttpd/lighttpd.conf /etc/lighttpd/lighttpd.conf.copy
+cp -v etc/lighttpd/lighttpd.conf /etc/lighttpd/lighttpd.conf 
 
-# sudo iptables -A OUTPUT -p tcp -m tcp --sport 80   -m state --state ESTABLISHED -j ACCEPT
-# sudo iptables -A OUTPUT -p tcp -m tcp --sport 443  -m state --state ESTABLISHED -j ACCEPT
+cp -v local/sbin/
+# sed -i 's:/var/www/html:/var/www/public:' /etc/lighttpd/lighttpd.conf
+# echo "server.error-handler-404 = \"/home.php\"" >> /etc/lighttpd/lighttpd.conf
+
+iptables -A INPUT -p tcp -m tcp --dport 80  -j ACCEPT
+iptables -A INPUT -p tcp -m tcp --dport 443 -j ACCEPT
+
+iptables -A OUTPUT -p tcp -m tcp --sport 80   -m state --state ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p tcp -m tcp --sport 443  -m state --state ESTABLISHED -j ACCEPT
+
 
 
 
