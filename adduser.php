4a1afa18eff70969ccdf762e1ec01abcf8056c22
nanku-dev <dev@nancoo.org>
2017-09-07 17:38:38 -0400

    Update and rename hash.php to adduser.php

---
+++ adduser.php
@@ -0,0 +1,21 @@
+<?php
+$username = 'user';  // EDIT THIS VALUE
+$password = 'value'; // EDIT THIS VALUE
+$options = ['cost' => 9,];
+$hash = password_hash($password, PASSWORD_BCRYPT, $options);
+
+try {
+    $pdo = new PDO('sqlite:/srv/sqlite3/data/user.db');
+} catch (EXCEPTION $e) { die("Unable to connect: " . $e->getMessage()); }
+                
+try {
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->beginTransaction();
+    $sql = 'INSERT INTO hash(value, user) VALUES(?,?)';
+    $statement = $pdo->prepare($sql);
+    $statement->execute(array($hash,$user));
+    $pdo->commit();
+    $pdo = null;
+} catch (EXCEPTION $e) { die("Unable to commit: " . $e->getMessage());}
+
+?>
--- hash.php
+++
@@ -1,5 +0,0 @@
-<?php
-$options = ['cost' => 9,];
-$hash = password_hash("password", PASSWORD_BCRYPT, $options);
-echo $hash;
-?>
